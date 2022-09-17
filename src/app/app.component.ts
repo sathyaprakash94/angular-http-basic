d2f46d450ec6ed8097de15b9f67272054d83db9c
sathyaprakash94 <sathyaprakash94@gmail.com>
2022-09-17 14:17:50 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'my-app',
@@ -8,13 +9,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
-  public posts: any;
+  public posts: Observable<any>;
   public sResponse: boolean;
 
   constructor(private http: HttpClient) {}
 
   getPosts() {
     this.sResponse = true;
-    this.posts = this.http.get(this.ROOT_URL + '/posts');
+    this.posts = this.http.get(this.ROOT_URL + '/posts?userId=1');
   }
 }
